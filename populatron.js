86c0af29caa3a42beddda684116ac2f1bb54b9fb
mavroian <mavflorin@gmail.com>
2019-04-18 23:24:02 +0900

    Finished populatron

--- populatron.js
+++ populatron.js
@@ -1,5 +1,28 @@
 const fs = require("fs");
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(callback) {
+    const csv = new Promise((resolve, reject) =>
+      fs.readFile("cities.csv", "utf8", (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      })
+    );
+    csv.then((data) => {
+      let array = data.split("\n");
+      let arr = [];
+      array.forEach((list) => {
+        arr.push(list.split(","));
+      });
+
+      arr.shift();
+      let max = 0;
+      arr.forEach((value) => {
+        max += Number(value[2]);
+      });
+      callback(max);
+    });
+  },
 };
